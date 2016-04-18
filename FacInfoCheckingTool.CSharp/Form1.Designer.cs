2e531d6fda8b07beb37468b3eafc02f1e3bec4be
heray1990 <heray1990@hotmail.com>
2016-04-18 16:43:37 +0800

    添加 From1 和 WatermarkTextBox 控件的代码。 其中 Form1 的代码并不完整，WatermarkTextBox 控件的代码做法可以参考下面的文章：http://blog.csdn.net/heray1990/article/details/51180081。

---
+++ FacInfoCheckingTool.CSharp/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace FacInfoCheckingTool.CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.串口设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.watermarkTextBoxBarcode = new FacInfoCheckingTool.CSharp.WatermarkTextBox();
+            this.watermarkTextBoxMacAddr = new FacInfoCheckingTool.CSharp.WatermarkTextBox();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.watermarkTextBoxMacAddr);
+            this.groupBox1.Controls.Add(this.watermarkTextBoxBarcode);
+            this.groupBox1.Location = new System.Drawing.Point(12, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(184, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "条码数据";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.设置ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 25);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 设置ToolStripMenuItem
+            // 
+            this.设置ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.串口设置ToolStripMenuItem});
+            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.设置ToolStripMenuItem.Text = "设置";
+            // 
+            // 串口设置ToolStripMenuItem
+            // 
+            this.串口设置ToolStripMenuItem.Name = "串口设置ToolStripMenuItem";
+            this.串口设置ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.串口设置ToolStripMenuItem.Text = "串口设置";
+            // 
+            // watermarkTextBoxBarcode
+            // 
+            this.watermarkTextBoxBarcode.Font = new System.Drawing.Font("SimSun", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.watermarkTextBoxBarcode.Location = new System.Drawing.Point(6, 20);
+            this.watermarkTextBoxBarcode.Name = "watermarkTextBoxBarcode";
+            this.watermarkTextBoxBarcode.Size = new System.Drawing.Size(170, 23);
+            this.watermarkTextBoxBarcode.TabIndex = 1;
+            this.watermarkTextBoxBarcode.WatermarkText = "请输入电视条码";
+            // 
+            // watermarkTextBoxMacAddr
+            // 
+            this.watermarkTextBoxMacAddr.Font = new System.Drawing.Font("SimSun", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.watermarkTextBoxMacAddr.Location = new System.Drawing.Point(6, 49);
+            this.watermarkTextBoxMacAddr.Name = "watermarkTextBoxMacAddr";
+            this.watermarkTextBoxMacAddr.Size = new System.Drawing.Size(170, 23);
+            this.watermarkTextBoxMacAddr.TabIndex = 2;
+            this.watermarkTextBoxMacAddr.WatermarkText = "请输入 MAC 地址";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "工厂信息检验工具";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 串口设置ToolStripMenuItem;
+        private WatermarkTextBox watermarkTextBoxBarcode;
+        private WatermarkTextBox watermarkTextBoxMacAddr;
+    }
+}
+
